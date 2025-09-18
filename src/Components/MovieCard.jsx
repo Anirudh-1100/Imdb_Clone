497de6be79aabd185db716abeb2cdc86601e607b
Anirudh <anirudhpatil2003@gmail.com>
2025-09-19 00:52:13 +0530

    updated

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MovieCard({ movie, name }) {
   return (
-    <div className="group relative h-[40vh] w-[200px] rounded-xl overflow-hidden cursor-pointer transform transition-transform duration-300 hover:scale-110 ">
+    <div className="group relative h-[40vh] w-[200px] rounded-xl overflow-hidden cursor-pointer transform transition-transform duration-300 hover:scale-110 flex flex-col">
       <div
         className="h-full w-full bg-center bg-cover"
         style={{ backgroundImage: `url(${movie.Poster})` }}
